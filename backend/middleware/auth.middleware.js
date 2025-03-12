a0e437777c3985dfa3f7c0c38c80b895f7d2223f
ArunKushhhh <arunkushwaha_230604@aitpune.edu.in>
2025-03-13 03:14:15 +0530

    backend/get all projects/add user/ get specific project by id
    Signed-off-by: ArunKushhhh <arunkushwaha_230604@aitpune.edu.in>

--- backend/middleware/auth.middleware.js
+++ backend/middleware/auth.middleware.js
@@ -5,7 +5,7 @@ export const authUser = async (req, res, next) => {
   try {
     const token =
       req.cookies.token ||
-      (req.headers.authorization && req.headers.authorization.split(" ")[1]);
+      req.headers.authorization.split(" ")[1];
 
     if (!token) {
       return res.status(401).send({ error: "Unauthorized user" });
--- backend/routes/project.routes.js
+++ backend/routes/project.routes.js
@@ -7,9 +7,29 @@ const router = Router();
 
 router.post(
   "/create",
-  authMiddleware.authUser,
+  authMiddleware.authUser, //allows only logged in users to create a project
   body("name").isString().withMessage("Name is required"),
   projectController.createProject
 );
+
+router.get(
+  "/all",
+  authMiddleware.authUser, //allows only logged in users to create a project
+  projectController.getAllProjects
+);
+
+router.put(
+  "/add-user",
+  authMiddleware.authUser, ////allows only logged in users to create a project
+  body("projectId").isString().withMessage("Project Id is required"),
+  body("users")
+    .isArray({ min: 1 })
+    .withMessage("Users must be an array of strings")
+    .custom((users) => users.every((user) => typeof user === "string"))
+    .withMessage("Each user must be a string"),
+  projectController.addUserToProject
+);
+
+router.get('/get-projects/:projectId', authMiddleware.authUser, projectController.getProjectById)
 
 export default router;
